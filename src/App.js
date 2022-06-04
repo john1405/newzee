097ce021da08adab62a08caf329a6b9f5e2e813b
john1405 <himanshuone00@gmail.com>
2022-06-04 15:13:23 +0530

    working newsgrid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import React, { Component } from 'react'
 import NavBar from './component/NavBar';
+import News from './component/News';
 
 export default class App extends Component {
   
@@ -9,6 +10,7 @@ export default class App extends Component {
     return (
       <div>
         <NavBar/>
+        <News/>
       </div>
 
     )
