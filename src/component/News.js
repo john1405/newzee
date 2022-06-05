b56ab6f4a0be198258d6b6bf8417c6599dfd9fcf
john1405 <himanshuone00@gmail.com>
2022-06-05 14:14:15 +0530

    component work

--- src/component/News.js
+++ src/component/News.js
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  constructor(){
+    super();
+    console.log("NewsItem insider");
+  }
   render() {
     return (
       <div className="container my-3">
         <h2>Newzee -- Top Bulletins </h2>
         <div className="row">
           <div className="col-md-4">
-            <NewsItem />
+            <NewsItem title="Nothing" description="just checking" imageUrl="https://imageio.forbes.com/specials-images/imageserve//629a1786657a8856c24fcbbb/0x0.jpg?format=jpg&width=1200" />
           </div>
           <div className="col-md-4">
             <NewsItem />
